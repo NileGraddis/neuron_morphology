b4773b730560c209bde398be6fbfc7205a4667ce
nilegraddis <nilegraddis@gmail.com>
2020-06-23 10:38:51 -0700

    tests for first_met

--- tests/snap_polygons/test_cortex_surfaces.py
+++ tests/snap_polygons/test_cortex_surfaces.py
@@ -7,6 +7,8 @@ import neuron_morphology.snap_polygons.cortex_surfaces as cs
 
 
 class TestStandalone(unittest.TestCase):
+    """Tests for functions operating on shapely objects
+    """
 
     def test_remove_duplicates(self):
         cases = [
@@ -37,6 +39,7 @@ class TestStandalone(unittest.TestCase):
         condition = lambda pt: Point(0, 0).distance(pt) <= 1
         cases = [
             [Point(0, -2), Point(0, 0), 0, Point(0, -1)],
+            [Point(0, -3), Point(0, 0), 0, Point(0, 0)],
             [Point(0, -4), Point(0, 0), 2, Point(0, -1)],
             [Point(0, -4), Point(0, -1), 10, Point(0, -1)],
             [Point(0, -1.5), Point(0, 0.5), 1, Point(0, -1)]
@@ -48,3 +51,33 @@ class TestStandalone(unittest.TestCase):
                     unmet, met, condition, iterations)
                 print(obtained.xy, expected.xy)
                 self.assertEqual(obtained, expected)
+
+
+class TestCompound(unittest.TestCase):
+    """Tests for functions operating on other cortex_surfaces functions
+    """
+
+    def test_first_met(self):
+        condition = lambda pt: Point(0, 0).distance(pt) <= 1
+        cases = [
+            [
+                [(-4, 0), (-3, 0), (-2, 0), (-1, 0), (0, 0)],
+                2,
+                3,
+                Point(-1, 0)
+            ],
+            [
+                [Point(0, 0), Point(1, 0), Point(2, 0)],
+                30,
+                0,
+                Point(0, 0)
+            ]
+        ]
+
+        for coords, iterations, idx_exp, coord_exp in cases:
+            with self.subTest():
+                idx_obt, coord_obt = cs.first_met(
+                    coords, condition, iterations)
+                print(idx_obt, idx_exp, coord_obt, coord_exp)
+                self.assertEqual(idx_obt, idx_exp)
+                self.assertEqual(coord_obt, coord_exp)
